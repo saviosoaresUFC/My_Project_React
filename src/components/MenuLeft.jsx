1b5ab05261b6b46b227350d58fbc3a42c11fa9f4
SavioUFC <saviocarvalho@alu.ufc.br>
2024-01-29 14:13:23 -0300

    edition file

--- src/components/MenuLeft.jsx
+++ src/components/MenuLeft.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import './Styles/MenuLeftStyle.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Col, Nav, Row, Tab, Navbar, NavDropdown } from 'react-bootstrap'
-import { DiGit, DiPostgresql, DiReact, DiCoffeescript } from "react-icons/di";
+import { Col, Nav, Row, Tab } from 'react-bootstrap'
+import { DiGit, DiPostgresql, DiReact } from "react-icons/di";
+import Lottie from 'react-lottie'
 
 
 const MenuLeft = () => {
+    const lottieGraph = {
+        loop: true,
+        autoplay: true,
+        animationData: require('../lottie/LottieGraph.json'),
+    };
+    const lottieGraph2 = {
+        loop: true,
+        autoplay: true,
+        animationData: require('../lottie/LottieGraph2.json'),
+    };
     return (
         <div>
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
-                <Row className='row'>
+                <Row>
                     <Col sm={1} className="order-3 order-sm-1">
                         <Nav variant="pills" className="flex-column">
                             <Nav.Item className='nav-item'>
-                                <Nav.Link className='nav-link' eventKey="first">PostgreSQL</Nav.Link>
+                                <Nav.Link className='nav-link' eventKey="first">Icons</Nav.Link>
                             </Nav.Item>
                             <Nav.Item className='nav-item'>
                                 <Nav.Link className='nav-link' eventKey="second">React</Nav.Link>
@@ -22,7 +33,7 @@ const MenuLeft = () => {
                                 <Nav.Link className='nav-link' eventKey="third">GIT</Nav.Link>
                             </Nav.Item>
                             <Nav.Item className='nav-item'>
-                                <Nav.Link className='nav-link' eventKey="fourth">JAVA</Nav.Link>
+                                <Nav.Link className='nav-link' eventKey="fourth">Lottie</Nav.Link>
                             </Nav.Item>
                         </Nav>
                     </Col>
@@ -30,6 +41,8 @@ const MenuLeft = () => {
                         <Tab.Content>
                             <Tab.Pane eventKey="first">
                                 <DiPostgresql className='icon' />
+                                <DiGit className='icon' />
+                                <DiReact className='icon' />
                             </Tab.Pane>
                             <Tab.Pane eventKey="second">
                                 <DiReact className='icon' />
@@ -38,7 +51,9 @@ const MenuLeft = () => {
                                 <DiGit className='icon' />
                             </Tab.Pane>
                             <Tab.Pane eventKey="fourth">
-                                <DiCoffeescript className='icon' />
+                                {/* <DiCoffeescript className='icon' /> */}
+                                <Lottie options={lottieGraph} width={150} height={150} />
+                                <Lottie options={lottieGraph2} width={150} height={150} />
                             </Tab.Pane>
                         </Tab.Content>
                     </Col>

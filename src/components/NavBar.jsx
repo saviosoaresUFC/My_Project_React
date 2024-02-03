9c84d19fe0872e1263ecd71b2265b4f4df174914
SavioUFC <saviocarvalho@alu.ufc.br>
2024-02-03 10:05:51 -0300

    edition file

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,51 +1,213 @@
 import React from 'react'
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 import Logo from '../img/logo_montenegro.png'
-import { Navbar, Nav, NavDropdown, Button } from 'react-bootstrap';
+import AvatarSavio from '../img/avatarsavio.jpg'
+import { Nav, NavDropdown, Button } from 'react-bootstrap';
 import './Styles/NavBarStyle.css'
+import { FaNetworkWired } from "react-icons/fa";
+import { FaHandshakeAngle } from "react-icons/fa6";
+import { IoBagSharp, IoDocumentText } from "react-icons/io5";
+import { GrServices } from "react-icons/gr";
+import { RxExit } from "react-icons/rx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Dropdown, Collapse, initMDB } from "mdb-ui-kit";
 
 export default function NavBar() {
+    initMDB({ Dropdown, Collapse });
     return (
-        <Navbar expand="lg" className="nav">
-            <Link to="/" className='icon'>
-                <img src={Logo} alt="logo" className='logo' />
-            </Link>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggler" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link href="#link" className="nav-link">
-                        <CustomLink to="/auxilios">Auxilios</CustomLink>
-                    </Nav.Link>
-                    <Nav.Link href="#home" className="nav-link">
-                        <CustomLink to="/bolsas">Bolsas</CustomLink>
-                    </Nav.Link>
-                    <Nav.Link href="#link" className="nav-link">
-                        <CustomLink to="/editais">Editais</CustomLink>
-                    </Nav.Link>
-                    <Nav.Link href="#link" className="nav-link">
-                        <CustomLink to="/intranet">Intranet</CustomLink>
-                    </Nav.Link>
-                    <Nav.Link href="#link" className="nav-link">
-                        <CustomLink to="/servicos">Servicos</CustomLink>
-                    </Nav.Link>
-                </Nav>
-                <Nav className='nav-buttons'>
-                    <Button variant="secondary" className='btn-exit'>SAIR</Button>
-                </Nav>
-            </Navbar.Collapse>
-        </Navbar>
+        <>
+            {/* <!-- Font Awesome --> */}
+            <link
+                href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+                rel="stylesheet"
+            />
+            {/* <!-- Google Fonts --> */}
+            <link
+                href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+                rel="stylesheet"
+            />
+            {/* <!-- MDB --> */}
+            <link
+                href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
+                rel="stylesheet"
+            />
+            {/* <!-- Sidebar --> */}
+            <Nav id="sidebarMenu" style={{marginTop: 62.6}} className="collapse d-lg-block sidebar bg-white">
+                <div className="position-sticky">
+                    <div className="list-group list-group-flush mx-3 mt-4">
+                        <Nav.Link className="nav-link">
+                            <CustomLink to="/auxilios">
+                                <FaHandshakeAngle size={20} className="me-3" />
+                                <strong>Auxilios</strong>
+                            </CustomLink>
+                        </Nav.Link>
+                        <Nav.Link className="nav-link">
+                            <CustomLink to="/bolsas">
+                                <IoBagSharp size={20} className="me-3" />
+                                <strong>Bolsas</strong>
+                            </CustomLink>
+                        </Nav.Link>
+                        <Nav.Link className="nav-link">
+                            <CustomLink to="/editais">
+                                <IoDocumentText size={20} className="me-3" />
+                                <strong>Editais</strong>
+                            </CustomLink>
+                        </Nav.Link>
+                        <Nav.Link className="nav-link">
+                            <CustomLink to="/intranet">
+                                <FaNetworkWired size={20} className="me-3" />
+                                <strong>Intranet</strong>
+                            </CustomLink>
+                        </Nav.Link>
+                        <Nav.Link className="nav-link">
+                            <CustomLink to="/servicos">
+                                <GrServices size={20} className="me-3" />
+                                <strong>Servicos</strong>
+                            </CustomLink>
+                        </Nav.Link>
+                        <Nav.Link className="nav-link">
+                            <CustomLink to="/">
+                                <RxExit size={20} className="me-3" />
+                                <strong>Sair</strong>
+                            </CustomLink>
+                        </Nav.Link>
+                    </div>
+                </div>
+            </Nav>
+            {/* <!-- Sidebar --> */}
+            {/* <!-- Navbar --> */}
+            <nav class="navbar navbar-expand-lg navbar-light bg-body-tertiary">
+                {/* <!-- Container wrapper --> */}
+                <div class="container-fluid">
+                    {/* <!-- Toggle button --> */}
+                    <button
+                        data-mdb-collapse-init
+                        class="navbar-toggler"
+                        type="button"
+                        data-mdb-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <i class="fas fa-bars"></i>
+                    </button>
+
+                    {/* <!-- Collapsible wrapper --> */}
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        {/* <!-- Navbar brand --> */}
+                        <a className="navbar-brand" href="#">
+                            <Link to="/" className='icon'>
+                                <img src={Logo} alt="logo" className='logo' />
+                            </Link>
+                        </a>
+                        {/* <!-- Left links --> */}
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Dashboard</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Team</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Projects</a>
+                            </li>
+                        </ul>
+                        {/* <!-- Left links --> */}
+                    </div>
+                    {/* <!-- Collapsible wrapper --> */}
+
+                    {/* <!-- Right elements --> */}
+                    <div class="d-flex align-items-center">
+                        {/* <!-- Icon --> */}
+                        <a class="link-secondary me-3" href="#">
+                            <i class="fas fa-shopping-cart"></i>
+                        </a>
+
+                        {/* <!-- Notifications --> */}
+                        <div class="dropdown">
+                            <a
+                                data-mdb-dropdown-init
+                                class="link-secondary me-3 dropdown-toggle hidden-arrow"
+                                href="#"
+                                id="navbarDropdownMenuLink"
+                                role="button"
+                                aria-expanded="false"
+                            >
+                                <i class="fas fa-bell"></i>
+                                <span class="badge rounded-pill badge-notification bg-danger">1</span>
+                            </a>
+                            <ul
+                                class="dropdown-menu dropdown-menu-end"
+                                aria-labelledby="navbarDropdownMenuLink"
+                            >
+                                <li>
+                                    <a class="dropdown-item" href="#">Some news</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Another news</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* <!-- Avatar --> */}
+                        <div class="dropdown">
+                            <a
+                                data-mdb-dropdown-init
+                                class="dropdown-toggle d-flex align-items-center hidden-arrow"
+                                href="#"
+                                id="navbarDropdownMenuAvatar"
+                                role="button"
+                                aria-expanded="false"
+                            >
+                                <img
+                                    src={AvatarSavio}
+                                    class="rounded-circle"
+                                    height="25"
+                                    alt="Black and White Portrait of a Man"
+                                    loading="lazy"
+                                />
+                            </a>
+                            <ul
+                                class="dropdown-menu dropdown-menu-end"
+                                aria-labelledby="navbarDropdownMenuAvatar"
+                            >
+                                <li>
+                                    <a class="dropdown-item" href="#">My profile</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Settings</a>
+                                </li>
+                                <li>
+                                    <a class="dropdown-item" href="#">Logout</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    {/* <!-- Right elements --> */}
+                </div>
+                {/* <!-- Container wrapper --> */}
+            </nav>
+            {/* <!-- Navbar --> */}
+            <script
+                type="text/javascript"
+                src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.umd.min.js"
+            ></script>
+        </>
     )
 }
+
 
 function CustomLink({ to, children, ...props }) {
     const resolvePath = useResolvedPath(to)
     const isActive = useMatch({ path: resolvePath.pathname, end: true })
 
     return (
-        <li className={isActive ? 'active' : ''}>
-            <Link to={to} {...props}>
+        <Link style={{ textDecoration: 'none' }} className={`${isActive ? 'text-active' : ''}`} to={to} {...props}>
+            <div className={`list-group-item ${isActive ? 'active' : ''} border-0`}>
                 {children}
-            </Link>
-        </li>
+            </div>
+        </Link>
     )
 }

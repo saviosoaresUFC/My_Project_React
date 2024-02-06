657a40188fabe12713e8ff2943d79f82407766f2
SavioUFC <saviocarvalho@alu.ufc.br>
2024-02-06 17:01:40 -0300

    edition file

--- src/components/ModalTermodeAdesao.jsx
+++ src/components/ModalTermodeAdesao.jsx
@@ -1,26 +1,29 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 export default function ModalTermodeAdesao(props) {
     return (
-        <Modal
-            {...props}
-            backdrop="static"
-            keyboard={false}
-        >
+        <Modal {...props}>
             <Modal.Header closeButton>
-                <Modal.Title>Modal title</Modal.Title>
+                <Modal.Title>Termo de Adesao</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                I will not close if you click outside me. Do not even try to press
-                escape key.
+                <div className='body-termo-adesao'>
+                    <p>As informações fornecidas à Plataforma OPP-UTFPR são de minha inteira responsabilidade,
+                        o que isenta terceiros de quaisquer garantias sobre a confiabilidade das informações registradas.
+                        Desde já autorizo a Plataforma OPP-UTFPR a realizar todas as providências que julgue necessárias para
+                        verificar a exatidão e a veracidade do que foi informado. Declaro, também, ter ciência do seguinte:</p>
+                    <ul>
+                        <li>Receberei, via e-mail, avisos encaminhados pela Plataforma que sejam de meu interesse; e</li>
+                        <li>Poderei receber convites para realizar avaliações como consultor da plataforma.</li>
+                    </ul>
+                </div>
             </Modal.Body>
             <Modal.Footer>
-                <Button variant="secondary" onClick={props.onHide()}>
+                <Button variant="outline-primary" onClick={props.onHide}>
                     Close
                 </Button>
-                <Button variant="primary">Understood</Button>
             </Modal.Footer>
         </Modal>
     )
